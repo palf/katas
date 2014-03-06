ca10606805112472b7bf5fea0c128403b31e925e
Jack Palfrey <jack.palf@gmail.com>
2014-03-06 09:26:35 +0000

    removed coin flip, added template for async dice throw

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var actions = require('./src/actionFactory');
 var execute = require('./src/executor');
 
-// var action = actions.flipCoin('heads');
-var action = actions.rollDice(4);
+// var action = actions.rollDice(4);
+var action = actions.rollDiceAsync(4);
 
 execute(action);
--- src/actionFactory.js
+++ src/actionFactory.js
@@ -4,27 +4,12 @@ function throwParade() { console.log('parade! :D') };
 function throwTantrum() { console.log('tantrum :(') };
 
 
-function coinMatches (value) {
-    return function () {
-        var flip = Math.random() > 0.5;
-        var side = flip ? 'heads' : 'tails'
-        console.log('flipped ' + side);
-        return side === value;
-    }
-}
-
 function rollMatches (value) {
     return function () {
         var roll = Math.floor( Math.random() * 6 ) + 1;
         console.log('rolled a ' + roll);
         return roll === value;
     }
-}
-
-
-exports.flipCoin = function (value) {
-    var flipCoin = coinMatches(value);
-    return new Action(flipCoin);
 }
 
 
@@ -36,3 +21,11 @@ exports.rollDice = function (value) {
     return action;
 }
 
+
+exports.rollDiceAsync = function (value) {
+    var rollDice = rollMatches(value);
+    var action = new Action(rollDice);
+    action.onSuccess = throwParade;
+    action.onFailure = throwTantrum;
+    return action;
+}
