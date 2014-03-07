eeca3c4f658caa19e589dc53be9253fe70576b77
Jack Palfrey <jack.palf@gmail.com>
2014-03-07 20:48:29 +0000

    removed unused executor object

--- src/executor.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (action) {
-    var result = false;
-
-    while (!result && action.retryCount > 0) {
-        action.retryCount -= 1;
-        result = action.execute();
-    }
-};
